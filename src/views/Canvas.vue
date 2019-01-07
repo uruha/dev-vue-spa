2d1d4dc9b6615389f94c27e5c96859fa0adfd36d
uruha <kochmusica@gmail.com>
2019-01-07 23:13:50 +0900

    dynamic add shape

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -1,28 +1,81 @@
-<template>
-  <v-stage :config="configKonva">
-    <v-layer>
-      <v-circle :config="configCircle"></v-circle>
-    </v-layer>
-  </v-stage>
+<template lang="pug">
+  div#canvas
+    input(
+      type="button"
+      value="add shape"
+      @click="addShape"
+    )
+    v-stage(:config="configKonva" ref="stage")
+      v-layer(ref="layer")
+        v-text(
+          :config="configText"
+          @dragstart="handleDragStart"
+          @dragend="handleDragEnd"
+        )
+        v-circle(
+          v-for="item in circleList"
+          :key="item.id"
+          :config="item"
+        )
 </template>
 
 <script>
+import Konva from 'konva';
+
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 400;
+
 export default {
   data() {
     return {
       configKonva: {
-        width: 200,
-        height: 200
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT
       },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: 'red',
-        stroke: 'black',
-        strokeWidth: 4
-      }
+      configGroup: {
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+        draggable: true
+      },
+      configText: {
+        text: 'Some text here',
+        x: 50,
+        y: 50,
+        fontSize: 20,
+        draggable: true,
+        fill: 'black'
+      },
+      circleList: []
     };
+  },
+  methods: {
+    handleDragStart() {
+      this.configText.text = 'dragging';
+    },
+    handleDragEnd() {
+      this.configText.text = 'Some text here';
+    },
+    addShape() {
+      const stage = this.$refs.stage.getStage();
+      const layers = stage.getLayers();
+      const layer = layers[0];
+      const circleConfig = {
+        x: Math.random() * 100,
+        y: Math.random() * 100,
+        radius: Math.random() * 100,
+        fill: Konva.Util.getRandomColor(),
+        stroke: 'black',
+        strokeWidth: Math.random() * 10,
+        draggable: true
+      };
+      console.log(this.$refs);
+      console.log(stage);
+      console.log(layer);
+
+      // layer.add(new Konva.Circle(circleConfig));
+      // layer.draw();
+      this.circleList.push(circleConfig);
+    }
   }
 };
 </script>
